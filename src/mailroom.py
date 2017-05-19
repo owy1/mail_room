9a2812c58aa62f6fc0c3f75c381f017303a62185
W. Ely Paysinger <paysinger@gmail.com>
2017-05-19 10:01:18 -0700

    logic finished

--- src/mailroom.py
+++ src/mailroom.py
@@ -8,6 +8,7 @@ try:
 except NameError:
     pass
 
+
 def create_donor_list():
     """Create donor list in dictionary."""
     fake = Factory.create()
@@ -18,53 +19,54 @@ def create_donor_list():
     return donor_dct
 
 
-def usr_prompt():
+def usr_prompt(donor_list):  # pragma: no cover
     """Prompt for user input."""
-    usr_input = input("""Please enter 1 to send a Thank You note or \n
-            enter 2 to create a donor report or enter 3 to exit system.""")
-    return usr_input
+    usr_input = input("""Please enter 1 to send a Thank You note
+enter 2 to create a donor report or
+enter 3 to exit system.""")
+    if usr_input == "1":
+        send_thank_you(donor_list)
+    elif usr_input == "2":
+        donor_report(donor_list)
+    elif usr_input == "3":
+        sys.exit()
+    else:
+        print("Please select 1, 2 or 3.")
+        usr_prompt(donor_list)
 
 
-def send_thank_you(donor_list):
+def send_thank_you(donor_list):  # pragma: no cover
     """Print thank you email."""
-    usr_input = input("""Please enter full name, example John Doe. or \n
-                      enter "list" for list of names or \n
-                      enter "back" to return to original prompt""")
+    usr_input = input("""Please enter full name, example John Doe. or
+enter "list" for list of names or
+enter "back" to return to original prompt""")
     if usr_input == "list":
         print(donor_list.keys())
-        usr_input()
+        send_thank_you()
     elif usr_input == "back":
-        usr_input()
+        usr_prompt()
     else:
         if usr_input not in donor_list.keys():
             donor_list[usr_input] = []
         donation_is_numeric = False
         while not donation_is_numeric:
-            donation = input("""Please enter donation amount
-                             rounded to nearest dollar.""")
+            donation = input(
+                """Please enter donation amount rounded to nearest dollar.""")
             if donation.isnumeric:
                 donor_list[usr_input].append(donation)
                 donation_is_numeric = True
-    print("{} {}".format(usr_input))
+    print("Thank you, {} for the donation of ${}!".format(usr_input, donation))
 
 
 def donor_report(donor_list):
     """Print donor report."""
-    pass
+    # print(sorted(donor_list.items()))
+    print(sorted(donor_list.items(), key=lambda kv: sum(kv[1]), reverse=True))
 
 
 def main():  # pragma: no cover
     donor_list = create_donor_list()
-    choice = usr_prompt()
-    if choice == "1":
-        send_thank_you(donor_list)
-    elif choice == "2":
-        donor_report(donor_list)
-    elif choice == "3":
-        sys.exit()
-    else:
-        print("Please select 1, 2 or 3.")
-        usr_prompt(donor_list)
+    usr_prompt(donor_list)
 
 
 main()

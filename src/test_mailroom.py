caa9334478a0c22782c58a0bed53dbb3b821d25d
W. Ely Paysinger <paysinger@gmail.com>
2017-05-18 20:24:44 -0700

    working on tests and tweeking logic

--- src/test_mailroom.py
+++ src/test_mailroom.py
@@ -1,45 +1,35 @@
 """This module tests mailroom.py."""
 import pytest
-import mailroom.py
 
-PARAMS_TABLE = [
-        (1, True),
-        (2, True),
-        (3, True),
-        (4, True)
-]
-
-
-def send_thank_you():
-    """Dummy var for 
-    return True
-
-
-def donor_report():
-    return True
-
-def  usr_input():
-    return True
 
 def test_create_donor_list_0_0():
     """Create donor list in dictionary."""
     from mailroom import create_donor_list
     assert len(create_donor_list()) == 20
 
+
 def test_create_donor_list_0_1():
     """First entry is two words."""
     from mailroom import create_donor_list
-    assert len(create_donor_list().items()[0][0].split(" ")) == 2
+    assert len(list(create_donor_list().keys())[0].split(" ")) == 2
 
-@pytest.mark.parametrize("m, result")
-def test_usr_input()_0_0:
-    """Test user input behavior."""
-    from mailroom import test_usr_input
+
+@pytest.mark.parametrize("fake_choice", [1, 2, 3, 4, "exit"])
+def test_usr_input(monkeypatch, fake_choice):
+    """Test user input behavior.
+        Using monkeypatch to put in input."""
+    from mailroom import usr_input
+    monkeypatch.setitem(__builtins__, 'input', lambda choice: fake_choice)
+    assert usr_input() == fake_choice
+
 
 def test_send_thank_you():
     """Print donor report."""
     from mailroom import send_thank_you
+    pass
+
 
 def test_donor_report():
     """Print donor report."""
     from mailroom import donor_report
+    pass

e668cb5f98cffd1da67c554186dd0c266e7ce19d
W. Ely Paysinger <paysinger@gmail.com>
2017-05-19 10:32:24 -0700

    Tox, setup and reqs

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='mailroom',
+    description="manages a donation 'database'",
+    version='0.1',
+    author="Ophelia and Ely",
+    license='MIT',
+    py_modules=['mailroom'],
+    package_dir={'': 'src'},
+    extras_require={'testing': ['pytest', 'pytest-cov']},
+    entry_points={'console_scripts': ['mailroom = mailroom:main']}
+)
